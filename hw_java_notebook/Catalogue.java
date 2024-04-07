02bdc84075ad23d6f1700545b37892f249720db2
gummydonkey <101453486+gummydonkey@users.noreply.github.com>
2024-04-07 22:17:58 +0500

    Delete hw_java_notebook directory

--- hw_java_notebook/Catalogue.java
+++
@@ -1,9 +0,0 @@
-package java.hw_java_notebook;
-
-public class Catalogue extends Notebook {
-
-    public Catalogue(int id, int ram, int hdd, String os, String color){
-        super(id,ram,hdd,os,color);
-    }
- 
-}
